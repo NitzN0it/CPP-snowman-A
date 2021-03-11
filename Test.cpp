6d2a2ab2114e99e7b97170cdfd3c1ef761142e68
NitzN0it <nitz@namer.us>
2021-03-11 10:44:35 +0200

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -14,7 +14,7 @@ using namespace ariel;
 #include <array>
 
 using namespace std;
-const array<string,4> hats = {"_===_"," ---\\n .....", "  -\\n  /_\\"," ---\\n (_*_)"};
+const array<string,4> hats = {"_===_","  ___ \n.....", "  -\n  /_\\"," ---\n (_*_)"};
 const array<string,4> eyes = {".","o","O","-"};
 const array<string,4> noses = {",",".","_"," "};
 const array<string,4> left_arm = {"<","\\","/"," "};
@@ -66,43 +66,38 @@ TEST_CASE("Unknown body part (not only 1-4 digits) and should throw an out of ra
 }
 TEST_CASE("Good snowman code") {
     for (int i = 0; i < 4; ++i) {
-        CHECK(snowman(MIN_SNOWMAN_SIZE*i*10000000) == " "+hats[i]+"\\n (.,.) \\n<( : )>\\n ( : )\\n"); //hats
+        CHECK(nospaces(snowman(MIN_SNOWMAN_SIZE+(i*10000000))) == nospaces(" "+hats[i]+"\n (.,.) \n<( : )>\n ( : )\n")); //hats
     }
     for (int i = 0; i < 4; ++i) {
-        CHECK(snowman(MIN_SNOWMAN_SIZE*i*1000000) == " _===_\\n (."+noses[i]+".) \\n<( : )>\\n ( : )\\n"); //noses
+        CHECK(nospaces(snowman(MIN_SNOWMAN_SIZE+(i*1000000))) == nospaces(" _===_\n (."+noses[i]+".) \n<( : )>\n ( : )\n")); //noses
     }
     for (int i = 0; i < 4; ++i) {
-        CHECK(snowman(MIN_SNOWMAN_SIZE*i*100000) == " _===_\\n ("+eyes[i]+",.) \\n<( : )>\\n ( : )\\n"); //left eye
+        CHECK(nospaces(snowman(MIN_SNOWMAN_SIZE+(i*100000))) == nospaces(" _===_\n ("+eyes[i]+",.) \n<( : )>\n ( : )\n")); //left eye
     }
     for (int i = 0; i < 4; ++i) {
-        CHECK(snowman(MIN_SNOWMAN_SIZE*i*10000) == " _===_\\n (.,"+eyes[i]+") \\n<( : )>\\n ( : )\\n"); //right eye
+        CHECK(nospaces(snowman(MIN_SNOWMAN_SIZE+(i*10000))) == nospaces(" _===_\n (.,"+eyes[i]+") \n<( : )>\n ( : )\n")); //right eye
     }
+
     for (int i = 0; i < 4; ++i) {
-        if (i < 3)
-            CHECK(snowman(MIN_SNOWMAN_SIZE*i*1000) == " _===_\\n (.,.) \\n"+left_arm[i]+"( : )>\\n ( : )\\n"); //left arm
+        if (i < 2)
+            CHECK(snowman(MIN_SNOWMAN_SIZE+(i*1000)) == " _===_\n (.,.) \n"+left_arm[i]+"( : )>\n ( : )\n"); //left arm
         else
-            CHECK(snowman(MIN_SNOWMAN_SIZE*i*1000) == " _===_\\n \\(.,.) \\\n( : )>\\n ( : )\\n"); //left arm up
+            CHECK(snowman(MIN_SNOWMAN_SIZE+(i*1000)) == " _===_\n \\(.,.) \\n( : )>\n ( : )\n"); //left arm up
 
     }
     for (int i = 0; i < 4; ++i) {
-        if (i < 3)
-            CHECK(snowman(MIN_SNOWMAN_SIZE*i*1000) == " _===_\\n (.,"") \\n<( : )"+right_arm[i]+"\\n ( : )\\n"); //right arm
+        if (i < 2)
+            CHECK(snowman(MIN_SNOWMAN_SIZE+(i*1000)) == " _===_\n (.,"") \n<( : )"+right_arm[i]+"\n ( : )\n"); //right arm
         else
-            CHECK(snowman(MIN_SNOWMAN_SIZE*i*1000) == " _===_\\n \\(.,.)/\\\n<( : )\\n ( : )\\n"); //right arm up
+            CHECK(snowman(MIN_SNOWMAN_SIZE+(i*1000)) == " _===_\n \\(.,.)/\\n<( : )\n ( : )\n"); //right arm up
 
     }
+
     for (int i = 0; i < 4; ++i) {
-        CHECK(snowman(MIN_SNOWMAN_SIZE*i*10) == " _===_\n (.,.) \n<("+torsos[i]+")>\n ( : )\n"); //torso
+        CHECK(nospaces(snowman(MIN_SNOWMAN_SIZE+i*10)) == nospaces(" _===_\n (.,.) \n<("+torsos[i]+")>\n ( : )\n")); //torso
     }
     for (int i = 0; i < 4; ++i) {
-        CHECK(snowman(MIN_SNOWMAN_SIZE*i) == " _===_\n (.,.) \n<( : )>\n ("+bases[i]+")\n"); //base
+        CHECK(nospaces(snowman(MIN_SNOWMAN_SIZE+i)) == nospaces(" _===_\n (.,.) \n<( : )>\n ("+bases[i]+")\n")); //base
     }
-    CHECK(nospaces(snowman(11114411)) == nospaces("_===_\\n(.,.)\\n( : )\\n( : )"));
-    CHECK(nospaces(snowman(33232124)) == nospaces("   _\\n  /_\\\\n\\(o_O)\\n (] [)>\\n (   )"));
-    CHECK(nospaces(snowman(12222212)) == nospaces(" _===_\\n\\(o.o)/\\n ( : ) \\n (\" \")"));
-    CHECK(nospaces(snowman(11111111)) == nospaces(" _===_ \\n (.,.) \\n<( : )>\\n ( : )"));
-    CHECK(nospaces(snowman(22222222)) == nospaces("  ___  \\n ..... \\n\\(o.o)/\\n (] [) \\n (\" \")"));
-    CHECK(nospaces(snowman(33333333)) == nospaces("   _   \\n  /_\\  \\n (O_O) \\n/(> <)\\\\n (___)"));
-    CHECK(nospaces(snowman(44444444)) == nospaces("  ___  \\n (_*_) \\n (- -) \\n (   ) \\n (   )"));
 }
 
